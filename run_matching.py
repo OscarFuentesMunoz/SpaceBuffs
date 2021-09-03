04eb299589a44bbd8663eac29a7c95d3016a3824
Shota Takahashi <takasho777@users.noreply.github.com>
2021-09-03 15:25:55 -0600

    Add a script to run the final matching

---
+++ run_matching.py
@@ -0,0 +1,192 @@
+from origin.ephemeris import Ephemeris
+import numpy as np
+import argparse
+import csv
+import os
+from bayes_opt import BayesianOptimization
+import time
+
+from numpy.lib.utils import info
+from origin.screening import Screen
+from origin.data_parser import debris_parser
+
+# Using Bayesian optimization, perform matching and output the solution into csv
+def main(fname_out, fname_est):
+
+    # Setup
+    time_very_beginning = time.time()
+    solution = []  # array of [debris_id, parent_id, cram]
+    logdir = "data/solution"
+    if not os.path.isdir(logdir):
+            os.makedirs(logdir)
+    
+    # Read existing estimation results
+    ids_estimated = []  # array of int
+    info_estimated = []  # array of dict
+    with open(os.path.join("data/results", fname_est)) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            ids_estimated.append(float(row[0]))
+            epoch_in_days = float(row[1])
+            state = np.array([
+                float(row[2]), float(row[3]), float(row[4]),
+                float(row[5]), float(row[6]), float(row[7]),
+                float(row[8]),
+            ])
+            sigmas = np.array([
+                float(row[9]), float(row[10]), float(row[11]),
+                float(row[12]), float(row[13]), float(row[14]),
+                float(row[15]),
+            ])
+            info_estimated.append({
+                "epoch": float(row[1]),
+                "state": state,
+                "sigmas": sigmas,
+            })
+
+    # If the output file already has entries, read these and skip computation
+    id_already_processed = []
+    try:
+        with open(os.path.join(logdir, fname_out)) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                id = int(float(row[0]))
+                sat_id = int(float(row[1]))
+                cram = float(row[2])
+                solution.append([id, sat_id, cram])
+                id_already_processed.append(id)
+        print("The entries for the following debris already exist. The computation will be skipped for those.")
+        print(id_already_processed)
+    except:
+        print("The output file does not contain any data.")
+        print("Starting from the debris id 1.")
+
+    # Setup screening parameters
+    screen = Screen()
+    R = np.diag([
+        50.0**2, 0.001**2, 0.003**2, 0.1**2, 0.1**2, 0.1**2,
+    ])
+
+    # Loop over 100 debris
+    for i in range(100):
+        # Check if the entry is alrady in the output file
+        id = i + 1
+        if id in id_already_processed:
+            continue  # => skip the entry
+        
+        print("Debris ID: {}".format(id))
+
+        # Define Bayesian optimization spec depending on whether the estimates are available or not. Need (obj_func, state_bounds, n_iter, init_points)
+        # Check if PF estimate exists
+        if id in ids_estimated:
+            # n_iter = 15
+            # init_points = 5
+            n_iter = 2
+            init_points = 0
+            ind = ids_estimated.index(id)
+            epoch_s = info_estimated[ind]["epoch"] * 86400.0
+            state = info_estimated[ind]["state"]  # cram is in m^2/kg
+            sigmas = info_estimated[ind]["sigmas"]
+            state_upper = np.zeros(7)
+            state_upper[:6] = state[:6] + sigmas[:6]
+            state_upper[6] = np.log10(state[6] + sigmas[6])
+            state_lower = np.zeros(7)
+            state_lower[:6] = state[:6] - sigmas[:6]
+            state_lower[6] = np.log10(state[6] - sigmas[6])
+        else:
+            n_iter = 5
+            init_points = 0
+            debris_data = debris_parser(deb_id=id, is_training=False)
+            ephem0 = debris_data["ephemeris"]
+            epoch_s = ephem0.time[0]
+            keplerian = ephem0.get_keplerian()[0]
+            sigmas_kep = np.array([
+                50.0, 0.001, 0.003, 0.1, 0.1, 0.1, 
+            ])
+            state_upper = np.zeros(7)
+            state_upper[:6] = keplerian + sigmas_kep
+            state_upper[6] = 1.8
+            state_lower = np.zeros(7)
+            state_lower[:6] = keplerian - sigmas_kep
+            state_lower[6] = -0.5
+
+        obj_func = lambda a, e, i, W, w, E, log_cram: \
+                -screen.argmin_rms(
+                epoch_s, a, e, i, W, w, E, 10**log_cram*1e-6, R
+            )[0]
+        state_bounds = {
+                "a": (state_lower[0], state_upper[0]),
+                "e": (state_lower[1], state_upper[1]),
+                "i": (state_lower[2], state_upper[2]),
+                "W": (state_lower[3], state_upper[3]),
+                "w": (state_lower[4], state_upper[4]),
+                "E": (state_lower[5], state_upper[5]),
+                "log_cram": (state_lower[6], state_upper[6]),
+            }
+
+
+        # Run Bayesian optimization
+        optimizer = BayesianOptimization(
+            f=obj_func,
+            pbounds=state_bounds,
+            random_state=1,
+        )
+        time_start = time.time()
+        optimizer.maximize(
+            init_points=init_points,
+            n_iter=n_iter,
+        )
+        time_stop = time.time()
+        print("Computation finished in {} min".format((time_stop - time_start)/60))
+        print("Cumulative computation time: {} min".format((time_stop - time_very_beginning)/60))
+
+
+        # Get corresponding satellite ID
+        opt_result = optimizer.max
+        state_opt = opt_result["params"]
+        screen_result = screen.argmin_rms(
+            epoch_s, 
+            state_opt["a"],
+            state_opt["e"],
+            state_opt["i"],
+            state_opt["W"], 
+            state_opt["w"], 
+            state_opt["E"], 
+            10**state_opt["log_cram"]*1e-6,
+            R
+        )
+        sat_id = screen_result[1]
+        cram = 10**state_opt["log_cram"]
+
+        print("Matched satellite id: {}".format(screen_result[1]))
+        print("Estimated Cr(A/m): {}".format(10**state_opt["log_cram"]*1e-6))
+        print("Estimated log10[Cr(A/m)]: {}".format(state_opt["log_cram"]))
+
+
+        # Save output to csv  (debris_id, parent_id, cram)
+        with open(os.path.join(logdir, fname_out), "a") as f:
+            writer = csv.writer(f)
+            writer.writerow([id, sat_id, cram])
+        solution.append([id, sat_id, cram])
+
+    # Save another text file for submission
+    solution_np = np.array(solution)
+    solution_np.sort(axis=0)
+    solution_np[:, 1:].savetxt(
+        os.path.join(
+            logdir, fname_out.strip(".csv") + "_for_submission.txt"
+        ),
+    )
+
+    return solution
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Perform matching and output solution.')
+    parser.add_argument('--name_out', type=str, default="solution_bayes.csv",
+                        help='Name of the output txt file.')
+    parser.add_argument('--name_source', type=str, default="pf_result.csv",
+                        help='Name of the csv file in data/results directory that contains state estimates.')
+    args = parser.parse_args()
+    main(args.name_out, args.name_source)
